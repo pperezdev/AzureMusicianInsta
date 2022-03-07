f98d0b8c9c10d901ac79ed9533473b664058b0a9
antdbs <antoinedubois14@gmail.com>
2022-03-07 17:55:26 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.97.0"
-    }
-  }
-}
-
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {
@@ -20,13 +11,13 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "example" {
-  name     = "azure-insta-resources"
+  name     = "rg75011dd01"
   location = "West Europe"
 }
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+  name                = "network"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   address_space       = ["10.0.0.0/16"]
